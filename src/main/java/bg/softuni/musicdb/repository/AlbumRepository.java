744ec8e313744938fea856a5d0c27e41cd466e83
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-01-28 18:40:58 +0200

    Music DB

---
+++ src/main/java/bg/softuni/musicdb/repository/AlbumRepository.java
@@ -0,0 +1,27 @@
+package bg.softuni.musicdb.repository;
+
+import bg.softuni.musicdb.entity.Album;
+import bg.softuni.musicdb.entity.User;
+import bg.softuni.musicdb.enums.ArtistName;
+import bg.softuni.musicdb.enums.Genre;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface AlbumRepository extends JpaRepository<Album, String> {
+
+    Optional<Album> findByName(Genre genre);
+
+     List<Album> findAllByArtist(ArtistName artist);
+
+     @Query("SELECT a FROM Album a")
+     List<Album> findAllAlbums();
+
+    Optional<Album> findByIdAndAddedFrom(String id, User user);
+
+
+}
