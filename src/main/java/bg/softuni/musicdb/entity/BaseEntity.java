efc9b91b8a3fd2050d503cdab88e7c7c663f3581
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-01-31 18:47:45 +0200

    Music DB

---
+++ src/main/java/bg/softuni/musicdb/entity/BaseEntity.java
@@ -0,0 +1,27 @@
+package bg.softuni.musicdb.entity;
+
+import jakarta.persistence.GeneratedValue;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import org.hibernate.annotations.GenericGenerator;
+
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(generator = "uuid-hibernate-generator")
+    @GenericGenerator(name = "uuid-hibernate-generator", strategy = "org.hibernate.id.UUIDGenerator")
+    private String id;
+
+    protected BaseEntity() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
