689b1ae46393955e533662a06420bacec75cbf1d
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-11-16 17:29:12 +0200

    Music DB

--- src/main/java/bg/softuni/musicdb/entity/Album.java
+++ src/main/java/bg/softuni/musicdb/entity/Album.java
@@ -3,12 +3,20 @@ package bg.softuni.musicdb.entity;
 import bg.softuni.musicdb.enums.Genre;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "albums")
 public class Album extends BaseEntity {
 
@@ -43,87 +51,4 @@ public class Album extends BaseEntity {
     @ManyToOne(optional = false)
     private User addedFrom;
 
-
-    public Album() {
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Integer getCopies() {
-        return copies;
-    }
-
-    public void setCopies(Integer copies) {
-        this.copies = copies;
-    }
-
-    public LocalDate getReleaseDate() {
-        return releaseDate;
-    }
-
-    public void setReleaseDate(LocalDate releaseDate) {
-        this.releaseDate = releaseDate;
-    }
-
-    public String getProducer() {
-        return producer;
-    }
-
-    public void setProducer(String producer) {
-        this.producer = producer;
-    }
-
-    public Genre getGenre() {
-        return genre;
-    }
-
-    public void setGenre(Genre genre) {
-        this.genre = genre;
-    }
-
-    public User getAddedFrom() {
-        return addedFrom;
-    }
-
-    public void setAddedFrom(User addedFrom) {
-        this.addedFrom = addedFrom;
-    }
-
-    public Artist getArtist() {
-        return artist;
-    }
-
-    public void setArtist(Artist artist) {
-        this.artist = artist;
-    }
 }
