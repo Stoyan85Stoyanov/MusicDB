721de7f20cf240714366631d6eeeb97161639685
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-11-17 17:03:27 +0200

    Music DB

--- src/main/java/bg/softuni/musicdb/controller/UserController.java
+++ src/main/java/bg/softuni/musicdb/controller/UserController.java
@@ -66,7 +66,7 @@ public class UserController {
 
         return "redirect:/login";
     }
-    //--------------------------------------------------
+
 
     @GetMapping("/login")
     public String login() {
