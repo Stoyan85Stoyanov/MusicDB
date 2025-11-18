6a473ac4dbbc8d76e225c6ae2e6145e3073950a5
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-11-18 17:43:07 +0200

    Music DB

--- src/main/java/bg/softuni/musicdb/entity/User.java
+++ src/main/java/bg/softuni/musicdb/entity/User.java
@@ -3,8 +3,14 @@ package bg.softuni.musicdb.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
 @Table(name = "users")
 public class User extends BaseEntity {
 
@@ -20,39 +26,4 @@ public class User extends BaseEntity {
        @Column(nullable = false, unique = true)
        private String email;
 
-
-    public User() {
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
