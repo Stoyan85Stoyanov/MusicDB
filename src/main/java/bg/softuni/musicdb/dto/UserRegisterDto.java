f3e6433df3d7c1de29d0ef26099ffb506a10dcf3
Stoyan85Stoyanov <stoyan.stoyanov.kz@mail.bg>
2025-11-18 17:43:37 +0200

    Music DB

--- src/main/java/bg/softuni/musicdb/dto/UserRegisterDto.java
+++ src/main/java/bg/softuni/musicdb/dto/UserRegisterDto.java
@@ -3,7 +3,15 @@ package bg.softuni.musicdb.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserRegisterDto {
 
     @NotBlank
@@ -26,47 +34,4 @@ public class UserRegisterDto {
     @Size(min = 5, max = 20)
     private String confirmPassword;
 
-
-    public UserRegisterDto() {
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getConfirmPassword() {
-        return confirmPassword;
-    }
-
-    public void setConfirmPassword(String confirmPassword) {
-        this.confirmPassword = confirmPassword;
-    }
 }
